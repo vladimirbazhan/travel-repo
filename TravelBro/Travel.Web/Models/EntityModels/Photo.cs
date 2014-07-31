767bec1bd2c1ae6fd1e9547bd6befe9fe1aa0acf
roman_trofymenko <roman_trofymenko@epam.com>
2014-07-31 16:19:43 +0300

    Added model files

---
+++ TravelBro/Travel.Web/Models/EntityModels/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WebApplication1.Models.IdentityModels;
+
+namespace WebApplication1.Models.EntityModels
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        public DateTime Published { get; set; }
+        public string ImagePath { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        public virtual Visit Visit { get; set; }
+    }
+}
---
+++ TravelBro/Travel.Web/Models/EntityModels/Visit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace WebApplication1.Models.EntityModels
+{
+    public class Visit
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+        public double Cost { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public virtual Place Place { get; set; }
+
+        public virtual Trip Trip { get; set; }
+
+        public virtual Collection<Comment> Comments { get; private set; }
+        public virtual Collection<Photo> Photos { get; private set; }
+    }
+}
