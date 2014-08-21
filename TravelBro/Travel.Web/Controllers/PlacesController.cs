c07264de8be5a7d5a243017cc6d2b11252ca554e
vladimirbazhan <vladimir.bazhan@gmail.com>
2014-08-21 13:07:16 +0300

    PlacesController added.

---
+++ TravelBro/Travel.Web/Controllers/PlacesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Models.EntityModels;
+using WebApplication1.Models.IdentityModels;
+
+namespace WebApplication1.Controllers
+{
+    public class PlacesController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET api/Place
+        public IEnumerable<Place> GetPlaces()
+        {
+            return db.Places.ToList();
+        }
+
+        // GET api/Place/5
+        [ResponseType(typeof(Place))]
+        public IHttpActionResult GetPlace(int id)
+        {
+            Place place = db.Places.Find(id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(place);
+        }
+
+        // PUT api/Place/5
+        public IHttpActionResult PutPlace(int id, Place place)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != place.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(place).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlaceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Place
+        [ResponseType(typeof(Place))]
+        public IHttpActionResult PostPlace(Place place)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Places.Add(place);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = place.Id }, place);
+        }
+
+        // DELETE api/Place/5
+        [ResponseType(typeof(Place))]
+        public IHttpActionResult DeletePlace(int id)
+        {
+            Place place = db.Places.Find(id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            db.Places.Remove(place);
+            db.SaveChanges();
+
+            return Ok(place);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PlaceExists(int id)
+        {
+            return db.Places.Count(e => e.Id == id) > 0;
+        }
+    }
+}
