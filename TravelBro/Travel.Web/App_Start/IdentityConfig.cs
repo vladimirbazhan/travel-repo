4db6bf629592c08a9577d998522cd365f2ba25ba
vladimirbazhan <vladimir.bazhan@gmail.com>
2015-01-14 16:35:10 +0200

    Requirements to password reduced (for easier development)

--- TravelBro/Travel.Web/App_Start/IdentityConfig.cs
+++ TravelBro/Travel.Web/App_Start/IdentityConfig.cs
@@ -30,10 +30,10 @@ namespace WebApplication1
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
