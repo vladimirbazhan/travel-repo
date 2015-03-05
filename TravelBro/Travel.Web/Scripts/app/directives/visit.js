b67e0f8d4fe94dfa43cde7931b16209688b15636
vladimir.bazhan <vladimir.bazhan@gmail.com>
2015-03-05 18:53:50 +0300

    code formatting applied

--- TravelBro/Travel.Web/Scripts/app/directives/visit.js
+++ TravelBro/Travel.Web/Scripts/app/directives/visit.js
@@ -1,38 +1,39 @@
-define(['./module'], function (directives) {
+define(['./module'], function(directives) {
     'use strict';
-    directives.directive('visit', [function () {
+    directives.directive('visit', [
+        function() {
 
-        var buildHtml = function(visit) {
-            return '<a class="list-group-item">' +
+            var buildHtml = function(visit) {
+                return '<a class="list-group-item">' +
                     '<bold><h4 class="list-group-item-heading">Visited: ' + (visit.Place ? visit.Place.name + ' (' + visit.Place.formatted_address + ')' : '') + '</h4></bold>' +
                     (visit.Description ? '<p class="list-group-item-text">' + visit.Description + '</p>' : '') +
                     '<p class="list-group-item-text">Spent money: ' + visit.Cost + '</p>' +
-                '</a>';
-        }
+                    '</a>';
+            };
+            return {
+                restrict: 'E',
+                template: '<div></div>',
+                replace: true,
+                link: function(scope, element, attrs) {
+                    var visit = element.data('visit').data;
 
-        return {
-            restrict: 'E',
-            template: '<div></div>',
-            replace: true,
-            link: function (scope, element, attrs) {
-                var visit = element.data('visit').data;
-
-                var map = new google.maps.Map(element.get()[0], {});
-                var service = new google.maps.places.PlacesService(map);
-                var exec = function() {
-                    service.getDetails({
-                        placeId: visit.GPlaceId
-                    }, function (place, status) {
-                        if (status == google.maps.places.PlacesServiceStatus.OK) {
-                            visit.Place = place;
-                            element.append(buildHtml(visit));
-                        } else {
-                            setTimeout(exec, 100);
-                        }
-                    });
+                    var map = new google.maps.Map(element.get()[0], {});
+                    var service = new google.maps.places.PlacesService(map);
+                    var exec = function() {
+                        service.getDetails({
+                            placeId: visit.GPlaceId
+                        }, function(place, status) {
+                            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                                visit.Place = place;
+                                element.append(buildHtml(visit));
+                            } else {
+                                setTimeout(exec, 100);
+                            }
+                        });
+                    };
+                    exec();
                 }
-                exec();
-            }
-        };
-    }]);
+            };
+        }
+    ]);
 });
