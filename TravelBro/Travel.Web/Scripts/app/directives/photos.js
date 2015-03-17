9dba9a3cb68d32db6616421732a6c0fc60fbac0a
vladimir.bazhan <vladimir.bazhan@gmail.com>
2015-03-17 13:33:21 +0300

    photos directive made independent on parent scope.

--- TravelBro/Travel.Web/Scripts/app/directives/photos.js
+++ TravelBro/Travel.Web/Scripts/app/directives/photos.js
@@ -2,29 +2,32 @@
     'use strict';
     directives.directive('photos', ['$location', function ($location) {
         return {
+            scope: {
+                photos:'='
+            },
             restrict: 'E',
             replace: true,
             template: '<div class="photos"></div>',
             link: function (scope, element, attrs) {
-                scope.$watch('trip.Photos', function (newVal, oldVal) {
+                scope.$watch('photos', function (newVal, oldVal) {
                     if (newVal) {
                         var colCount = 4;
                         var imgIndex = 0;
-                        var rowCount = Math.ceil(scope.trip.Photos.length / colCount);
+                        var rowCount = Math.ceil(scope.photos.length / colCount);
                         for (var iRow = 0; iRow < rowCount; ++iRow) {
                             var row = $('<div class="row"></div>');
                             for (var jCol = 0; jCol < colCount; jCol++) {
                                 imgIndex = iRow * colCount + jCol;
-                                if (imgIndex >= scope.trip.Photos.length) {
+                                if (imgIndex >= scope.photos.length) {
                                     break;
                                 }
                                 var imgPath = $location.protocol() + '://' + $location.host() + ':' + $location.port() + '/' + 'api/trips/photos/';
-                                var img = '<img src="' + imgPath + scope.trip.Photos[imgIndex] + '/' + '" class="img-responsive img-rounded center-block" />';
+                                var img = '<img src="' + imgPath + scope.photos[imgIndex] + '/' + '" class="img-responsive img-rounded center-block" />';
                                 var col = $('<div class="col-md-3">' + img + '</div>');
                                 row.append(col);
                             }
                             element.append(row);
-                            if (imgIndex >= scope.trip.Photos.length) {
+                            if (imgIndex >= scope.photos.length) {
                                 break;
                             }
                         }
